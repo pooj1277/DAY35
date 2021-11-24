f456c16ac799362cc1efccc6e888ce9e5b2f7eb3
pooj1277 <pooja.sd3@gmail.com>
2021-11-24 01:04:03 -0800

    [Refactored] test cases for employee payroll

--- java/com/bridgelabz/JDBC/DBConnectivity.java
+++ java/com/bridgelabz/JDBC/DBConnectivity.java
@@ -25,32 +25,5 @@ public class DBConnectivity {
 
 }
 
-/*
- * public static void main(String[] args) {
 
-		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
-		String userName = "root";
-		String password = "Qwerty@ 1277";
-		Connection con;
-		try {
-
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver loaded");
-
-		} catch (ClassNotFoundException e) {
-			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
-		}
-		listDrivers();
-
-		try {
-
-			System.out.println("Connecting to database" + jdbcURL);
-			con = DriverManager.getConnection(jdbcURL, userName, password);
-			System.out.println("Connection is successfull!!!!" + con);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
- * */
  
